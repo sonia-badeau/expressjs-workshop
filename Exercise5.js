a694fab0f90aaef6521e26f2538f63ea4bbb09e1
Sonia <sonia.badeau@gmail.com>
2016-02-17 01:14:01 +0000

    Exercise 5 + Exercise 6 + challenge

---
+++ Exercise5.js
@@ -0,0 +1,35 @@
+var createNewContent = require("./sequelizer.js").createNewContent
+var findLatestPost = require('./sequelizer.js').findLatestPost;
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser')
+app.use(bodyParser());
+
+
+
+app.get('/', function(req, res) {
+    findLatestPost().then(function(posts){
+        res.send(posts);
+    })
+})
+
+
+// GET method route
+app.get('/createcontent', function(req, res){
+    res.sendFile(path.join(__dirname + '/form.html'));
+    
+});
+
+
+// POST method route
+// This route receives the posted form
+app.post('/createContent', function(req, res){
+    createNewContent(1, req.body.url, req.body.title, function (newContent) {
+        res.redirect('/');
+    })
+})
+
+
+app.listen(process.env.PORT);
+
